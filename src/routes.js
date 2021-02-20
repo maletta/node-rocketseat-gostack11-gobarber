349390b158356d460b9ca46a093ae4d93bc1b7f1
Mauricio Maletta <mauricio_maletta@hotmail.com>
2021-02-19 23:13:06 -0300

    create file uploads with multer

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionControler from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
@@ -6,6 +8,7 @@ import UserController from './app/controllers/UserController';
 import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const uploads = multer(multerConfig);
 
 routes.post('/sessions', SessionControler.store);
 routes.post('/users', UserController.store);
@@ -13,5 +16,9 @@ routes.post('/users', UserController.store);
 routes.use(AuthMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', uploads.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
